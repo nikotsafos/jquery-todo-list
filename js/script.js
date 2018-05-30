6aa16afe2445c1928c694fece664b6eb240b130f
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-05-30 09:14:37 -0700

    clears inputbox after you add item

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function addItem(e){
 	e.preventDefault();
 	var formData = $('form').serializeArray();
 	$('.list').append("<li>" + formData[0].value + "<input type='button' class='delete' value='delete' id='delete' /></li>");
-	$('input')[0].value = '';
+	$('.input')[0].value = '';
 	$(".label").focus();
 	$(document).on('click', '.delete', function() {
 		$(this).parent().remove();
