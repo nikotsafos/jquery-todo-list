864d4026ddd1c20d7281ee89d55e86e5b49f5b0e
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-05-30 08:35:30 -0700

    fixed delete button

--- js/script.js
+++ js/script.js
@@ -6,12 +6,13 @@ $(document).ready(function() {
 function addItem(e){
 	e.preventDefault();
 	var formData = $('form').serializeArray();
-	$('.list').append("<li>" + formData[0].value + "</li>" + "<button type='submit' id='delete'>Delete</button>");
+	$('.list').append("<li>" + formData[0].value + "<input type='button' class='delete' value='delete' id='delete' /></li>");
 	$('input')[0].value = '';
+	$(".label").focus();
+	$(document).on('click', '.delete', function() {
+		$(this).parent().remove();
+	});
 
 }
 
 
-$(document).on('click', '#delete', function() {
-	$(this).parent().remove();
-	});
